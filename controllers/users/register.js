3a1352dc5173aa20ed9d18013bc82970417c2489
Daria Tkachenko <113775461+dariaaferreira@users.noreply.github.com>
2023-08-07 23:01:18 +0300

    done hw06-email

--- controllers/users/register.js
+++ controllers/users/register.js
@@ -4,7 +4,9 @@ const gravatar = require('gravatar');
 
 const { User } = require('../../models/user');
 
-const { HttpError } = require('../../helpers');
+const { HttpError, sendEmail } = require('../../helpers');
+
+const { BASE_URL } = process.env;
 
 
 const register = async(req, res) => {
@@ -17,8 +19,17 @@ const register = async(req, res) => {
 
     const hashPassword = await bcrypt.hash(password, 10);
     const avatarURL = gravatar.url(email);
+    const verificationToken = Date.now().toString();
 
-    const newUser = await User.create({...req.body, password: hashPassword, avatarURL});
+
+    const newUser = await User.create({...req.body, password: hashPassword, avatarURL, verificationToken});
+    const verifyEmail ={
+        to: email,
+        subject: 'Verify email',
+        html: `<a target="_blank" href="${BASE_URL}/users/verify/${verificationToken}">Click verify email</a>`
+    };
+
+    await sendEmail(verifyEmail);
 
     res.status(201).json({
         email: newUser.email,
